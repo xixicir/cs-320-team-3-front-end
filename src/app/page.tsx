b64be72eb286a6b98b610deb8482b5deeecd6084
alexj3141 <alexjin3.14@gmail.com>
2023-04-24 14:43:25 -0400

    updated homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
+"use client"
 import React from "react";
+import { useRouter } from "next/navigation";
+import { isLoggedIn, deleteAuthToken } from "./auth";
 
 const HomePage = () => {
+  const router = useRouter();
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
       <div className="text-center">
         <h1 className="text-5xl font-bold mb-4">Time Tracker</h1>
         <p className="text-xl">Welcome to the Time Tracker application!</p>
-        <a href="/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded w-full">
+        {!isLoggedIn() ? <button onClick={()=>{router.push("/login")}} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded w-full">
           Proceed to Login Page
-        </a>
+        </button> : null}
+        {!isLoggedIn() ? <button onClick={()=>{router.push("/signup")}} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded w-full">
+          Proceed to Sign Up Page
+        </button> : null}
       </div>
     </div>
   );
