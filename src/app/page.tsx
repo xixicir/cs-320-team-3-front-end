63f4fb53b51dca459ae636f54231ff2d08ad74ee
alexj3141 <alexjin3.14@gmail.com>
2023-04-26 06:56:14 -0400

    made it so that when logging in it takes you to home page which gives you option to go to ur personalized page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,14 @@ const HomePage = () => {
         <p className="text-xl">Welcome to the Time Tracker application!</p>
         {!isLoggedIn() ? <button onClick={()=>{router.push("/login")}} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded w-full">
           Proceed to Login Page
-        </button> : null}
+          </button> : <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded w-full">
+          Proceed to your home page!
+          </button> 
+        }
         {!isLoggedIn() ? <button onClick={()=>{router.push("/signup")}} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded w-full">
           Proceed to Sign Up Page
-        </button> : null}
+          </button> : null
+        }
       </div>
     </div>
   );
