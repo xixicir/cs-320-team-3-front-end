1b72edf85a235fa0b22b91d5b031ef680034eff1
alexj3141 <alexjin3.14@gmail.com>
2023-04-26 05:23:14 -0400

    improved logout

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -2,9 +2,22 @@
 
 import { useRouter } from "next/navigation";
 import { deleteAuthToken } from "../auth";
+import React, { useEffect, useState } from "react";
+
+const LogoutPage = () => {
+  
+  const router = useRouter();
+
+  useEffect(() => {
+    deleteAuthToken();
+    router.push("/");
+  }, []);
+
+  return (<div>
+
+  </div>)
+}
 
 export default function Page() {
-  deleteAuthToken();
-  const router = useRouter();
-  router.push("/");
+  return <LogoutPage />
 }
