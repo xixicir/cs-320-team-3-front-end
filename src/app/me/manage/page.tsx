3af301b4812b12f03ae3e6c3641b5df50e69c2ab
alexj3141 <alexjin3.14@gmail.com>
2023-05-21 23:01:20 -0400

    reincorporated sign in and add managee features

--- src/app/me/manage/page.tsx
+++ src/app/me/manage/page.tsx
@@ -171,6 +171,10 @@ export default function Page() {
                       ))}
                     </tbody>
                   </table>
+                  <button onClick = {() => (router.push("/me/manage/add_managee"))}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
+                      Add New Managee
+                  </button>
                   <div className="flex justify-center mt-4">
                   </div>
                 </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export const Taskbar = () => {
           </div>
           <div className="flex space-x-4">
             <div
-              onClick={() => {deleteAuthToken(); router.push("/login")}}
+              onClick={() => {router.push("/logout")}}
               className="cursor-pointer text-gray-600 hover:text-gray-900 transition-colors duration-150 py-1 px-4 rounded hidden sm:block"
             >
               Logout
@@ -54,9 +54,34 @@ export const Taskbar = () => {
 
 export default function Page() {
   const router = useRouter();
-  if (!isLoggedIn()) {
-    router.push("/login")
-  } else {
-    router.push("/me")
-  }
+
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-indigo-500 to-blue-700 flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow-md w-96">
+      <h1 className="text-center text-4xl font-bold mb-8 text-indigo-700 tracking-tight">
+            Welcome to TimePunch!
+      </h1>
+      <div className="flex flex-col space-y-4">
+        {(!isLoggedIn()) ? <button
+          onClick={() => {router.push("/login")}}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+        >
+          Proceed to Login
+        </button> : <button
+        onClick={() => {router.push("/me")}}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
+          Proceed to Your Home Page!
+          </button>}
+          {
+            (!isLoggedIn()) ? <button
+            onClick={() => {router.push("/signup")}}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+            >
+              Proceed to Sign Up
+            </button> : <div></div>
+          }
+      </div>
+    </div>
+  </div>
+);
 }
